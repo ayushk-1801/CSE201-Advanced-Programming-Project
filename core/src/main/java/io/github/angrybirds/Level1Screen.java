4c37794c4b87971130caf76bdcf0ece5ee67beb2
jashn23256 <147977519+jashn23256@users.noreply.github.com>
2024-10-21 12:18:25 +0530

    Add files via upload

---
+++ core/src/main/java/io/github/angrybirds/Level1Screen.java
@@ -0,0 +1,94 @@
+package io.github.angrybirds;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+public class Level1Screen implements Screen {
+    private SpriteBatch batch;
+    private Texture bgImage;
+    private Stage stage;
+
+    // Images for the fort, pig, and slingshot
+    private Image pig;
+    private Image woodVertical1, woodVertical2, woodHorizontal;
+    private Image slingshot;
+
+    @Override
+    public void show() {
+        batch = new SpriteBatch();
+        bgImage = new Texture("ui/level_bg.png"); // Background for the level
+
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+
+        // Load textures for the slingshot, pig, and fort elements
+        Texture pigTexture = new Texture("assets/birds_piggies/normal_pig.png");
+        Texture woodVerticalTexture = new Texture("assets/materials/vertical wood block.png");
+        Texture woodHorizontalTexture = new Texture("assets/materials/horizontal wood block.png");
+        Texture slingshotTexture = new Texture("assets/slingshot.png");
+
+        // Create the pig and position it inside the fort
+        pig = new Image(pigTexture);
+        pig.setPosition(Gdx.graphics.getWidth() / 2f - pig.getWidth() / 2, Gdx.graphics.getHeight() / 2f);
+
+        // Create two vertical wood blocks (fort sides)
+        woodVertical1 = new Image(woodVerticalTexture);
+        woodVertical2 = new Image(woodVerticalTexture);
+        woodVertical1.setPosition(Gdx.graphics.getWidth() / 2f - 80, pig.getY() - 30);
+        woodVertical2.setPosition(Gdx.graphics.getWidth() / 2f + pig.getWidth() - 30, pig.getY() - 30);
+
+        // Create a horizontal wood block (fort top)
+        woodHorizontal = new Image(woodHorizontalTexture);
+        woodHorizontal.setPosition(Gdx.graphics.getWidth() / 2f - 20, pig.getY() + pig.getHeight() - 10);
+
+        // Create the slingshot and position it on the left side of the screen
+        slingshot = new Image(slingshotTexture);
+        slingshot.setPosition(100, Gdx.graphics.getHeight() / 2f - slingshot.getHeight() / 2);
+
+        // Add actors to the stage
+        stage.addActor(slingshot);
+        stage.addActor(woodVertical1);
+        stage.addActor(woodVertical2);
+        stage.addActor(woodHorizontal);
+        stage.addActor(pig);
+    }
+
+    @Override
+    public void render(float delta) {
+        batch.begin();
+        batch.draw(bgImage, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.end();
+
+        stage.act(delta);
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        bgImage.dispose();
+        stage.dispose();
+    }
+}
