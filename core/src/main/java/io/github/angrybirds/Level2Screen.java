729b1949a7059d0e6c354ec34644163b58f3fd17
jashn23256 <147977519+jashn23256@users.noreply.github.com>
2024-10-25 17:30:38 +0530

    Update Level2Screen.java

--- core/src/main/java/io/github/angrybirds/Level2Screen.java
+++ core/src/main/java/io/github/angrybirds/Level2Screen.java
@@ -13,60 +13,67 @@ public class Level2Screen implements Screen {
     private Texture bgImage;
     private Stage stage;
 
-    // Textures for pigs and blocks
-    private Image pig1, pig2, pig3;
-    private Image woodVertical1, woodVertical2, woodVertical3;
-    private Image woodHorizontal1, woodHorizontal2;
+    // Images for the pigs, blocks, and slingshot
+    private Image pig1, pig2, helmetPig; // One pig is a helmet pig
+    private Image woodVertical1, woodVertical2, woodHorizontal;
+    private Image slingshot;
 
     @Override
     public void show() {
         batch = new SpriteBatch();
-        bgImage = new Texture("ui/level_bg.png"); // Background for Level 2
+        bgImage = new Texture("background/level_bg.png"); // Background for the level
 
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
 
-        // Load textures for pigs and blocks
-        Texture pigTexture = new Texture("assets/birds_piggies/normal_pig.png");
-        Texture helmetPigTexture = new Texture("assets/birds_piggies/helmet_pig.png");
+        // Load textures for the slingshot, pigs, and fort elements
+        Texture pigTexture = new Texture("birds_piggies/normal_pig.png");
+        Texture helmetPigTexture = new Texture("birds_piggies/helmet_pig.png"); // Helmet pig texture
+        Texture woodVerticalTexture = new Texture("materials/vertical_wood.png");
+        Texture woodHorizontalTexture = new Texture("materials/horizontal_wood.png");
+        Texture slingshotTexture = new Texture("birds_piggies/slingshot.png");
 
-        Texture woodVerticalTexture = new Texture("assets/materials/vertical_wood.png");
-        Texture woodHorizontalTexture = new Texture("assets/materials/horizontal_wood.png");
-
-        // Create 3 pigs and position them
+        // Create the pigs and position them
         pig1 = new Image(pigTexture);
+        pig1.setPosition(Gdx.graphics.getWidth() / 2f - pig1.getWidth() / 2, Gdx.graphics.getHeight() / 2f);
+        pig1.moveBy(400, -400);
+
+        helmetPig = new Image(helmetPigTexture); // Helmet pig
+        helmetPig.setPosition(Gdx.graphics.getWidth() / 2f + 60, Gdx.graphics.getHeight() / 2f);
+        helmetPig.moveBy(400, -400);
+        helmetPig.setSize(helmetPig.getWidth()/2,helmetPig.getHeight()/2);
+
         pig2 = new Image(pigTexture);
-        pig3 = new Image(helmetPigTexture);
+        pig2.setPosition(Gdx.graphics.getWidth() / 2f + pig2.getWidth() / 2, Gdx.graphics.getHeight() / 2f);
+        pig2.moveBy(350, -200);
 
-        pig1.setPosition(Gdx.graphics.getWidth() / 2f - 150, Gdx.graphics.getHeight() / 2f);
-        pig2.setPosition(Gdx.graphics.getWidth() / 2f + 50, Gdx.graphics.getHeight() / 2f);
-        pig3.setPosition(Gdx.graphics.getWidth() / 2f - 50, Gdx.graphics.getHeight() / 2f - 100);
-
-        // Create and position vertical wooden blocks
+        // Create two vertical wood blocks (fort sides)
         woodVertical1 = new Image(woodVerticalTexture);
         woodVertical2 = new Image(woodVerticalTexture);
-        woodVertical3 = new Image(woodVerticalTexture);
+        woodVertical1.setPosition(Gdx.graphics.getWidth() / 2f - 80, pig1.getY() - 30);
+        woodVertical2.setPosition(Gdx.graphics.getWidth() / 2f + pig1.getWidth() - 30, pig1.getY() - 30);
+        woodVertical1.moveBy(400, 35);
+        woodVertical2.moveBy(400, 35);
 
-        woodVertical1.setPosition(Gdx.graphics.getWidth() / 2f - 180, pig1.getY() - 30);
-        woodVertical2.setPosition(Gdx.graphics.getWidth() / 2f + 90, pig2.getY() - 30);
-        woodVertical3.setPosition(Gdx.graphics.getWidth() / 2f - 50, pig3.getY() - 130);
+        // Create a horizontal wood block (fort top)
+        woodHorizontal = new Image(woodHorizontalTexture);
+        woodHorizontal.setPosition(Gdx.graphics.getWidth() / 2f - 20, pig1.getY() + pig1.getHeight() - 10);
+        woodHorizontal.moveBy(310, 115);
 
-        // Create and position horizontal wooden blocks (like shelves)
-        woodHorizontal1 = new Image(woodHorizontalTexture);
-        woodHorizontal2 = new Image(woodHorizontalTexture);
-
-        woodHorizontal1.setPosition(Gdx.graphics.getWidth() / 2f - 160, pig1.getY() + pig1.getHeight() - 10);
-        woodHorizontal2.setPosition(Gdx.graphics.getWidth() / 2f - 160, pig3.getY() + pig3.getHeight() + 10);
+        // Create the slingshot and position it on the left side of the screen, scaled down to 1/5th size
+        slingshot = new Image(slingshotTexture);
+        slingshot.setSize(slingshot.getWidth() / 5, slingshot.getHeight() / 5); // Scale down
+        slingshot.setPosition(100, Gdx.graphics.getHeight() / 2f - slingshot.getHeight() / 2);
+        slingshot.moveBy(200, -330);
 
         // Add actors to the stage
-        stage.addActor(pig1);
-        stage.addActor(pig2);
-        stage.addActor(pig3);
+        stage.addActor(slingshot);
         stage.addActor(woodVertical1);
         stage.addActor(woodVertical2);
-        stage.addActor(woodVertical3);
-        stage.addActor(woodHorizontal1);
-        stage.addActor(woodHorizontal2);
+        stage.addActor(woodHorizontal);
+        stage.addActor(pig1);
+        stage.addActor(helmetPig); // Added helmet pig to stage
+        stage.addActor(pig2);
     }
 
     @Override
