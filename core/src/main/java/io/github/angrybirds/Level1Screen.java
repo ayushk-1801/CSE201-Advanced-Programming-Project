52577e21a9ee689b935bb13e41edd71162258f12
ayushk-1801 <ayush23162@iiitd.ac.in>
2024-11-26 16:53:50 +0530

    Revert "chuck ability working again"
    This reverts commit 68b0c0cad5f01b552727a85da39fd7b678c9cd1d.

--- core/src/main/java/io/github/angrybirds/Level1Screen.java
+++ core/src/main/java/io/github/angrybirds/Level1Screen.java
@@ -347,7 +347,7 @@ public class Level1Screen implements Screen, ContactListener {
                 if (launched && TimeUtils.nanoTime() - launchTime > 500000000L) {
                     Vector2 currentVelocity = currentBirdBody.getLinearVelocity();
 
-                    Vector2 newVelocity = currentVelocity.scl(4f);
+                    Vector2 newVelocity = currentVelocity.scl(2f);
 
                     currentBirdBody.setLinearVelocity(newVelocity);
 
@@ -392,8 +392,6 @@ public class Level1Screen implements Screen, ContactListener {
                     currentBirdBody.getLinearVelocity().x * 0.8f,
                     currentBirdBody.getLinearVelocity().y
                 );
-                Vector2 newVelocity = currentBirdBody.getLinearVelocity().scl(5f);
-                currentBirdBody.setLinearVelocity(newVelocity);
                 timeOfContact = TimeUtils.nanoTime();
             }
         }
